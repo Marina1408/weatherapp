1d778b3258469afde8e41b7357ae5c1f2053a34a
Marina1408 <marina1408@ukr.net>
2019-01-09 13:56:48 +0200

    Get temperature from accuweather

--- weatherapp.py
+++ weatherapp.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
 
 """ Weather add project.
-
-This is my first project. I hope it will work!
 """
 
-print("Hellow. I'm alive!")
+import html
+from urllib.request import urlopen, Request
 
+AKKU_URL = "https://www.accuweather.com/uk/ua/rivne/325590/weather-forecast/325590"
+
+# getting page from server
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
+accu_request = Request(AKKU_URL, headers = headers)
+accu_page = urlopen(accu_request).read()
+accu_page = str(accu_page)
+
+ACCU_TEMP_TAG = '<span class="large-temp">'
+accu_temp_tag_size = len(ACCU_TEMP_TAG)
+accu_temp_tag_index = accu_page.find(ACCU_TEMP_TAG)
+accu_temp_value_start = accu_temp_tag_index + accu_temp_tag_size
+accu_temp = ''
+for char in accu_page[accu_temp_value_start:]:
+	if char != '<':
+		accu_temp += char
+	else:
+		break
+
+print('AccuWeather: \n')
+print(f'Temperature: {html.unescape(accu_temp)}\n')
 
 
