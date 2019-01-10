bd70cb3d2699e004f5c74333becb9ee42091871e
Marina1408 <marina1408@ukr.net>
2019-01-10 19:45:30 +0200

    The program show the weather conditions

--- weatherapp.py
+++ weatherapp.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python3
 
 """ Weather add project.
@@ -6,7 +7,7 @@
 import html
 from urllib.request import urlopen, Request
 
-AKKU_URL = "https://www.accuweather.com/uk/ua/rivne/325590/weather-forecast/325590"
+AKKU_URL = "https://www.accuweather.com/en/ua/rivne/325590/weather-forecast/325590?lang=en-us"
 
 # getting page from server
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
@@ -25,7 +26,23 @@ for char in accu_page[accu_temp_value_start:]:
 	else:
 		break
 
+ACCU_COND_TAG = '<span class="cond">'
+accu_cond_tag_size = len(ACCU_COND_TAG)
+accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
+accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
+accu_cond = u''
+for char in accu_page[accu_cond_value_start:]:
+	if char != '<':
+		accu_cond += char
+	else:
+		break
+
 print('AccuWeather: \n')
 print(f'Temperature: {html.unescape(accu_temp)}\n')
+print(f'Weather conditions: {html.unescape(accu_cond)}\n')
+
+
+
+
 
 
