bea2eecfd4e55c209fc3aafdeae1ddd52800b160
Marina1408 <marina1408@ukr.net>
2019-01-12 14:38:04 +0200

    The weather condition show in Ukraine, not in English

--- weatherapp.py
+++ weatherapp.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/python3
 
 """ Weather add project.
@@ -7,13 +6,13 @@
 import html
 from urllib.request import urlopen, Request
 
-AKKU_URL = "https://www.accuweather.com/en/ua/rivne/325590/weather-forecast/325590?lang=en-us"
+AKKU_URL = "https://www.accuweather.com/uk/ua/rivne/325590/weather-forecast/325590"
 
 # getting page from server
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 accu_request = Request(AKKU_URL, headers = headers)
 accu_page = urlopen(accu_request).read()
-accu_page = str(accu_page)
+accu_page = str(accu_page.decode())
 
 ACCU_TEMP_TAG = '<span class="large-temp">'
 accu_temp_tag_size = len(ACCU_TEMP_TAG)
@@ -30,7 +29,7 @@ ACCU_COND_TAG = '<span class="cond">'
 accu_cond_tag_size = len(ACCU_COND_TAG)
 accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
 accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
-accu_cond = u''
+accu_cond = ''
 for char in accu_page[accu_cond_value_start:]:
 	if char != '<':
 		accu_cond += char
