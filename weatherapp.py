9bc062d5c527e9e0b15f00518cbf0750146dbe04
Marina1408 <marina1408@ukr.net>
2019-01-14 14:58:24 +0200

    Corrected the length of the rp5_URL

--- weatherapp.py
+++ weatherapp.py
@@ -12,7 +12,7 @@ AKKU_URL = "https://www.accuweather.com/uk/ua/rivne/325590/weather-forecast/3255
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 accu_request = Request(AKKU_URL, headers = headers)
 accu_page = urlopen(accu_request).read()
-accu_page = str(accu_page.decode())
+accu_page = accu_page.decode('utf-8')
 
 ACCU_TEMP_TAG = '<span class="large-temp">'
 accu_temp_tag_size = len(ACCU_TEMP_TAG)
@@ -38,15 +38,18 @@ for char in accu_page[accu_cond_value_start:]:
 
 print('AccuWeather: \n')
 print(f'Temperature: {html.unescape(accu_temp)}\n')
-print(f'Weather conditions: {html.unescape(accu_cond)}\n')
+print(f'Weather conditions: {accu_cond}\n')
 
-rp5_URL = "http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%A0%D1%96%D0%B2%D0%BD%D0%BE%D0%BC%D1%83,_%D0%A0%D1%96%D0%B2%D0%BD%D0%B5%D0%BD%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C"
+rp5_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%'
+	       'A0%D1%96%D0%B2%D0%BD%D0%BE%D0%BC%D1%83,_%D0%A0%D1%96%D0%B2%D0%'
+	       'BD%D0%B5%D0%BD%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B'
+	       '0%D1%81%D1%82%D1%8C')
 
 # getting page from server
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 rp5_request = Request(rp5_URL, headers = headers)
 rp5_page = urlopen(rp5_request).read()
-rp5_page = str(rp5_page.decode())
+rp5_page = rp5_page.decode('utf-8')
 
 rp5_TEMP_TAG = '<span class="t_0" style="display: block;">'
 rp5_temp_tag_size = len(rp5_TEMP_TAG)
@@ -70,9 +73,9 @@ for char in rp5_page[rp5_cond_value_start:]:
 	else:
 		break
 
-print('rp5: \n')
+print('rp5.ua: \n')
 print(f'Temperature: {html.unescape(rp5_temp)}\n')
-print(f'Weather conditions: {html.unescape(rp5_cond)}\n')
+print(f'Weather conditions: {rp5_cond}\n')
 
 
 
