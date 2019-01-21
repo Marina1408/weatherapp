1e8e9f068c51ea7a8658bee6f13d95dfd842c02c
Marina1408 <marina1408@ukr.net>
2019-01-21 14:07:56 +0200

    Improved code using functions.

--- weatherapp.py
+++ weatherapp.py
@@ -6,124 +6,100 @@
 import html
 from urllib.request import urlopen, Request
 
-AKKU_URL = "https://www.accuweather.com/uk/ua/rivne/325590/weather-forecast/325590"
+ACCU_URL = "https://www.accuweather.com/uk/ua/rivne/325590/weather-forecast/325590"
+ACCU_TAGS = ('<span class="large-temp">', '<span class="cond">')
+ACCU_CONTAINER_TAG = ('\n')
 
-# getting page from server
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
-accu_request = Request(AKKU_URL, headers = headers)
-accu_page = urlopen(accu_request).read()
-accu_page = accu_page.decode('utf-8')
-
-ACCU_TEMP_TAG = '<span class="large-temp">'
-accu_temp_tag_size = len(ACCU_TEMP_TAG)
-accu_temp_tag_index = accu_page.find(ACCU_TEMP_TAG)
-accu_temp_value_start = accu_temp_tag_index + accu_temp_tag_size
-accu_temp = ''
-for char in accu_page[accu_temp_value_start:]:
-	if char != '<':
-		accu_temp += char
-	else:
-		break
-
-ACCU_COND_TAG = '<span class="cond">'
-accu_cond_tag_size = len(ACCU_COND_TAG)
-accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
-accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
-accu_cond = ''
-for char in accu_page[accu_cond_value_start:]:
-	if char != '<':
-		accu_cond += char
-	else:
-		break
-
-print('AccuWeather: \n')
-print(f'Temperature: {html.unescape(accu_temp)}\n')
-print(f'Weather conditions: {accu_cond}\n')
-
-rp5_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%'
+RP5_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%'
 	       'A0%D1%96%D0%B2%D0%BD%D0%BE%D0%BC%D1%83,_%D0%A0%D1%96%D0%B2%D0%'
 	       'BD%D0%B5%D0%BD%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B'
 	       '0%D1%81%D1%82%D1%8C')
-
-# getting page from server
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
-rp5_request = Request(rp5_URL, headers = headers)
-rp5_page = urlopen(rp5_request).read()
-rp5_page = rp5_page.decode('utf-8')
-
-rp5_TEMP_TAG = '<span class="t_0" style="display: block;">'
-rp5_temp_tag_size = len(rp5_TEMP_TAG)
-rp5_temp_tag_index = rp5_page.find(rp5_TEMP_TAG)
-rp5_temp_value_start = rp5_temp_tag_index + rp5_temp_tag_size
-rp5_temp = ''
-for char in rp5_page[rp5_temp_value_start:]:
-	if char != '<':
-		rp5_temp += char
-	else:
-		break
-
-
-WINFO_CONTAINER_TAG = '<div class="ArchiveInfo">'
-rp5_COND_TAG = '°F</span>'
-rp5_cond_tag_size = len(rp5_COND_TAG)
-rp5_cond_tag_index = rp5_page.find(rp5_COND_TAG,
-	                               rp5_page.find(WINFO_CONTAINER_TAG)) + 2
-rp5_cond_value_start = rp5_cond_tag_index + rp5_cond_tag_size
-rp5_cond = ''
-for char in rp5_page[rp5_cond_value_start:]:
-	if char != '<':
-		rp5_cond += char
-	else:
-		break
-
-rp5_COND_TAG2 = 'Бфрт)</span>'
-rp5_cond_tag_size = len(rp5_COND_TAG2)
-rp5_cond_tag_index = rp5_page.find(rp5_COND_TAG2,
-	                               rp5_page.find(WINFO_CONTAINER_TAG)) 
-rp5_cond_value_start = rp5_cond_tag_index + rp5_cond_tag_size
-rp5_cond2 = ''
-for char in rp5_page[rp5_cond_value_start:]:
-	if char != '<':
-		rp5_cond2 += char
-	else:
-		break
-
-print('rp5.ua: \n')
-print(f'Temperature: {html.unescape(rp5_temp)}\n')
-print(f'Weather conditions: {rp5_cond+rp5_cond2}\n')
+RP5_TAGS = ('<span class="t_0" style="display: block;">', '°F</span>')
+RP5_CONTAINER_TAG = ('<div class="ArchiveInfo">', '\n')
 
 
 SINOPTIK_URL = ('https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%'
 	            'D0%B0-%D1%80%D1%96%D0%B2%D0%BD%D0%B5')
+SINOPTIK_TAGS = ('<p class="today-temp">', '<div class="description"> <!--noindex--> ')
+SINOPTIK_CONTAINER_TAG = ('\n')
 
-# getting page from server
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
-sinoptik_request = Request(SINOPTIK_URL, headers = headers)
-sinoptik_page = urlopen(sinoptik_request).read()
-sinoptik_page = sinoptik_page.decode('utf-8')
 
-SINOPTIK_TEMP_TAG = '<p class="today-temp">'
-sinoptik_temp_tag_size = len(SINOPTIK_TEMP_TAG)
-sinoptik_temp_tag_index = sinoptik_page.find(SINOPTIK_TEMP_TAG)
-sinoptik_temp_value_start = sinoptik_temp_tag_index + sinoptik_temp_tag_size
-sinoptik_temp = ''
-for char in sinoptik_page[sinoptik_temp_value_start:]:
-	if char != '<':
-		sinoptik_temp += char
+def get_request_headers():
+	"""Getting headers of the request.
+	"""
+
+	return {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
+
+def get_page_source(url):
+	"""Getting page from server.
+	"""
+
+	request = Request(url, headers = get_request_headers())
+	page_source = urlopen(request).read()
+	return page_source.decode('utf-8')
+
+def get_tag_content(page_content, tag, container_tag):
+	"""Getting text from the page with tag.
+	"""
+	def get_container_tag_content(page_content, tag, container_tag):
+		"""Getting text from the page with additional tags.
+		"""
+
+		i = 0
+		while i < len(container_tag):
+			container = container_tag[i]
+			if page_content.count(container) >= 2:
+				i += 1
+			else:
+				break
+
+		tag_index = page_content.find(tag, page_content.find(container))
+		return tag_index
+
+	if page_content.count(tag) > 2:
+		tag_index = get_container_tag_content(page_content, tag, container_tag)
 	else:
-		break
+		tag_index = page_content.find(tag)
 
-SINOPTIK_COND_TAG = '<div class="description"> <!--noindex--> '
-sinoptik_cond_tag_size = len(SINOPTIK_COND_TAG)
-sinoptik_cond_tag_index = sinoptik_page.find(SINOPTIK_COND_TAG)
-sinoptik_cond_value_start = sinoptik_cond_tag_index + sinoptik_cond_tag_size
-sinoptik_cond = ''
-for char in sinoptik_page[sinoptik_cond_value_start:]:
-	if char != '<':
-		sinoptik_cond += char
-	else:
-		break
+	tag_size = len(tag)
+	value_start = tag_index + tag_size
+	content = ''
+	for c in page_content[value_start:]:
+		if c != '<':
+			content += c
+		else:
+			break
+	return content
 
-print('SINOPTIK.ua: \n')
-print(f'Temperature: {html.unescape(sinoptik_temp)}\n')
-print(f'Weather conditions: {sinoptik_cond}\n')
+def get_weather_info(page_content, tags, container_tag):
+	"""Getting the final result in tuple.
+	"""
+
+	return tuple([get_tag_content(page_content, tag, container_tag) for tag in tags])
+
+def produse_output(provider_name, temp, condition):
+	"""Displays the final result of the program
+	"""
+
+	print(f'\n{provider_name}:\n')
+	print(f'Temperature: {html.unescape(temp)}\n')
+	print(f'Weather conditions: {condition}\n')
+
+
+def main():
+	"""Main entry point.
+	"""
+
+	weather_sites = {"AccuWeather": (ACCU_URL, ACCU_TAGS, ACCU_CONTAINER_TAG),
+	                 "RP5": (RP5_URL, RP5_TAGS, RP5_CONTAINER_TAG),
+	                 "SINOPTIK.UA": (SINOPTIK_URL, SINOPTIK_TAGS, SINOPTIK_CONTAINER_TAG)}
+	for name in weather_sites:
+		url, tags, container_tag = weather_sites[name]
+		content = get_page_source(url)
+		temp, condition = get_weather_info(content, tags, container_tag)
+		produse_output(name, temp, condition)                 
+
+
+if __name__ == '__main__':
+	main()
+
