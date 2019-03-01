f09e4fa2ab6957e97fdc156518a5b2dcb051ea29
Marina1408 <marina1408@ukr.net>
2019-03-01 16:31:40 +0200

    Added exceptions defined by the users.

---
+++ exception.py
@@ -0,0 +1,43 @@
+""" App exceptions
+"""
+
+class ProgramError(Exception):
+
+	name = 'ProgramError'
+
+	def __init__(self, data, name1):
+		self.data = data
+		self.name1 = name1
+
+	def action(self):
+		print(f'{self.name}, {self.name1}: {self.data}')
+
+
+class WeatherProviderError(ProgramError):
+	
+	name = 'WeatherProviderError'
+
+
+class RequestError(WeatherProviderError):
+
+	name = 'RequestError'
+
+	def action(self):
+		print(f'{self.name}: {self.data}. Location = {self.name1} ???')
+
+
+class ConfigParserError(WeatherProviderError):
+	
+	name = 'ConfigParserError'
+
+	def action(self):
+		print(f'{self.name}: {self.data}{self.name1}')
+				
+
+class AppRunError(ProgramError):
+	
+	name = 'AppRunError'
+
+	
+
+
