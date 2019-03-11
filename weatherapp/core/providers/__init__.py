49b1f3db7382c29ff65e89261a3d90aeb68a5d3f
Marina1408 <marina1408@ukr.net>
2019-03-11 12:06:55 +0200

    Separate providers in their respective packages

--- weatherapp/core/providers/__init__.py
+++
@@ -1,3 +0,0 @@
-from weatherapp.core.providers.accu import AccuWeatherProvider
-from weatherapp.core.providers.rp5 import Rp5WeatherProvider
-from weatherapp.core.providers.sinoptik import SinoptikWeatherProvider
