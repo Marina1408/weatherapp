111f8c787422d795dbf42b3d6dcf4084d323892c
Marina1408 <marina1408@ukr.net>
2019-03-11 12:03:25 +0200

    Fixed entry_points.

--- weatherapp/core/providermanager.py
+++ weatherapp/core/providermanager.py
@@ -1,19 +1,23 @@
-from weatherapp.core.providers import (AccuWeatherProvider, 
-	                                   Rp5WeatherProvider, 
-                                       SinoptikWeatherProvider)
+import logging
+import pkg_resources
+
+from weatherapp.core import config
 from weatherapp.core import commandmanager
 
 class ProviderManager(commandmanager.CommandManager):
-	""" Discovers registered providers and loads them.
+	""" Discovers registered providers and loads them from an entrypoint.
 	"""
+
+	logger = logging.getLogger(__name__)
 
 	def _load_commands(self):
 		""" Loads all existing providers.
 		"""
 
-		for provider in [AccuWeatherProvider, 
-		                 Rp5WeatherProvider,
-		                 SinoptikWeatherProvider]:
-		    self.add(provider.name, provider)
+		entry_points = pkg_resources.iter_entry_points(
+			                         config.PROVIDER_EP_NAMESPACE)
+		for entry_point in entry_points:
+			self.logger.debug('found provider %r', entry_point.name)
+			self._commands[entry_point.name] = entry_point.load()
 
 	
