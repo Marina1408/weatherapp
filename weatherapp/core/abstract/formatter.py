f13ed27f36fa38c63c74e7aca77a92926aeedb5d
Marina1408 <marina1408@ukr.net>
2019-03-14 11:11:44 +0200

    Base abstract class for formatters was created.

---
+++ weatherapp/core/abstract/formatter.py
@@ -0,0 +1,18 @@
+import abc
+
+
+class Formatter(abc.ABC):
+	
+	""" Base abstract class for formatters.
+	"""
+
+	@abc.abstractmethod
+	def emit(self, column_names, data):
+		""" Format and print data from the iterable source.
+
+		:param column_names: names of the columns
+		:type column_names: list
+		:param data: iterable data source, one tuple per object
+		             with values in order of column names
+		:type data: list or tuple
+		"""
