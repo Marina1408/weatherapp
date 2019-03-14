60d5bcda2b68e6287ec1d928148e0b032ca8face
Marina1408 <marina1408@ukr.net>
2019-03-14 10:58:24 +0200

    Output data in table format.

---
+++ weatherapp/core/formatters/table.py
@@ -0,0 +1,33 @@
+import prettytable
+
+from weatherapp.core.abstract import Formatter
+
+
+class TableFormatter(Formatter):
+
+    """ Table formatter for app output.
+    """
+
+    def emit(self, column_names, data):
+        """ Format and print data from the iterable source.
+
+        :param column_names: names of the columns
+        :type column_names: list
+        :param data: iterable data source, one tuple per object
+                     with values in order of column names
+        :type data: list or tuple
+        """
+
+        pt = prettytable.PrettyTable()
+
+        for column, values in zip(column_names, (data.keys(), data.values())):
+        	if any(values):
+        		pt.add_column(column, list(values))
+
+        pt.align = 'l'
+        pt.adding_width = 1
+        pt.hrules = 0
+        pt.vrules = 1
+        return pt.get_string()
+
+
