1c75473d5fb701e09b6bda075692be1c43fa481a
Marina1408 <marina1408@ukr.net>
2019-03-14 15:40:39 +0200

    Added the 'managers' directory.

---
+++ weatherapp/core/managers/providermanager.py
@@ -0,0 +1,23 @@
+import logging
+import pkg_resources
+
+from weatherapp.core import config
+from weatherapp.core.managers import commandmanager
+
+class ProviderManager(commandmanager.CommandManager):
+	""" Discovers registered providers and loads them from an entrypoint.
+	"""
+
+	logger = logging.getLogger(__name__)
+
+	def _load_commands(self):
+		""" Loads all existing providers.
+		"""
+
+		entry_points = pkg_resources.iter_entry_points(
+			                         config.PROVIDER_EP_NAMESPACE)
+		for entry_point in entry_points:
+			self.logger.debug('found provider %r', entry_point.name)
+			self._commands[entry_point.name] = entry_point.load()
+
+	
