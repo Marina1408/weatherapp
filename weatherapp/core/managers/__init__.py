cc3ef47fec4e2870aaaa7ce8edd5cb75eda868c0
Marina1408 <marina1408@ukr.net>
2019-03-14 15:53:28 +0200

    Manager directory was created.

---
+++ weatherapp/core/managers/__init__.py
@@ -0,0 +1,3 @@
+from weatherapp.core.managers.commandmanager import CommandManager
+from weatherapp.core.managers.providermanager import ProviderManager
+from weatherapp.core.managers.formattermanager import FormatterManager
